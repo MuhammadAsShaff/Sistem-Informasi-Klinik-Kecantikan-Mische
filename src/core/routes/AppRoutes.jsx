ca2e88c416604c1d75597b0506cbd1e9215a724c
bintang puspita dewi <bintang22si@mahasiswa.pcr.ac.id>
2026-06-30 02:31:23 +0700

    Mengubah route

--- src/core/routes/AppRoutes.jsx
+++ src/core/routes/AppRoutes.jsx
@@ -13,49 +13,49 @@ import { Routes, Route } from "react-router-dom";
  */
 
 // Layout
-import CustomerLayout from "@/view/Customer/CustomerLayout/page/Index";
-import AdminLayout from "@/view/admin/AdminLayout/page/Index";
+import CustomerLayout from "@/view/Customer/CustomerLayout/page";
+import AdminLayout from "@/view/admin/AdminLayout/page";
 
 // Customer Pages
 import LandingPage from "@/view/Customer/LandingPage/page/landingPage";
-import HalamanPromo from "@/view/Customer/HalamanPromo/page/Index";
+import HalamanPromo from "@/view/Customer/HalamanPromo/page";
 import PromoDetail from "@/view/Customer/HalamanPromo/page/PromoDetail";
-import DetailJenisPerawatan from "@/view/Customer/DetailJenisPerawatan/page/Index";
-import ReservasiPage from "@/view/Customer/ReservasiPage/page/Index";
-import HalamanEvent from "@/view/Customer/HalamanEvent/page/Index";
+import DetailJenisPerawatan from "@/view/Customer/DetailJenisPerawatan/page";
+import ReservasiPage from "@/view/Customer/ReservasiPage/page";
+import HalamanEvent from "@/view/Customer/HalamanEvent/page";
 import EventDetail from "@/view/Customer/HalamanEvent/page/EventDetail";
-import LoginPage from "@/view/authentication/Login/page/Index";
-import RegistrasiPage from "@/view/authentication/Registrasi/page/Index";
-import TentangKamiPage from "@/view/Customer/TentangKami/page/Index";
+import LoginPage from "@/view/authentication/Login/page";
+import RegistrasiPage from "@/view/authentication/Registrasi/page";
+import TentangKamiPage from "@/view/Customer/TentangKami/page";
 import TentangDokterDropdown from "@/view/Customer/TentangDokter/page/TentangDokterDropdown";
 import DetailDokterPage from "@/view/Customer/TentangDokter/page/DetailDokter";
-import HalamanTestimoni from "@/view/Customer/HalamanTestimoni/page/Index";
+import HalamanTestimoni from "@/view/Customer/HalamanTestimoni/page";
 import DetailTestimoni from "@/view/Customer/HalamanTestimoni/page/DetailTestimoni";
-import ProfilCustomerPage from "@/view/Customer/ProfilCustomer/page/Index";
+import ProfilCustomerPage from "@/view/Customer/ProfilCustomer/page";
 import RiwayatReservasiPage from "@/view/Customer/ProfilCustomer/page/RiwayatReservasi";
 import RiwayatPembelianPage from "@/view/Customer/ProfilCustomer/page/RiwayatPembelian";
-import HalamanProduk from "@/view/Customer/HalamanProduk/page/Index";
+import HalamanProduk from "@/view/Customer/HalamanProduk/page";
 import DetailProduk from "@/view/Customer/HalamanProduk/page/DetailProduk";
-import DetailKeranjang from "@/view/Customer/DetailKeranjang/page/Index";
+import DetailKeranjang from "@/view/Customer/DetailKeranjang/page";
 
 // Admin Pages
-import AdminDashboard from "@/view/admin/dashboard/page/Index";
-import KelolaUser from "@/view/admin/KelolaUser/page/Index";
-import KelolaJadwalReservasiTreatment from "@/view/admin/KelolaJadwalReservasiTreatment/page/Index";
-import KelolaProfilKlinik from "@/view/admin/KelolaProfilKlinik/page/Index";
-import KelolaProfilAdmin from "@/view/admin/KelolaProfilAdmin/page/Index";
-import KelolaTestimoni from "@/view/admin/KelolaTestimoni/page/Index";
-import KelolaProduk from "@/view/admin/KelolaProduk/page/Index";
-import KelolaPenjualan from "@/view/admin/KelolaPenjualan/page/Index";
+import AdminDashboard from "@/view/admin/dashboard/page";
+import KelolaUser from "@/view/admin/KelolaUser/page";
+import KelolaJadwalReservasiTreatment from "@/view/admin/KelolaJadwalReservasiTreatment/page";
+import KelolaProfilKlinik from "@/view/admin/KelolaProfilKlinik/page";
+import KelolaProfilAdmin from "@/view/admin/KelolaProfilAdmin/page";
+import KelolaTestimoni from "@/view/admin/KelolaTestimoni/page";
+import KelolaProduk from "@/view/admin/KelolaProduk/page";
+import KelolaPenjualan from "@/view/admin/KelolaPenjualan/page";
 
 // Core Routing & Shared Components
 import ProtectedRoute from "./ProtectedRoute";
-import KelolaProfilDokter from "@/view/admin/KelolaProfilDokter/page/Index";
-import KelolaPromo from "@/view/admin/KelolaPromo/page/Index";
-import KelolaEvent from "@/view/admin/KelolaEvent/page/Index";
-import KelolaReservasi from "@/view/admin/KelolaReservasi/page/Index";
-import KelolaKategoriProduk from "@/view/admin/KelolaKategoriProduk/page/Index";
-import Page404 from "@/view/components/Page404/page/Index";
+import KelolaProfilDokter from "@/view/admin/KelolaProfilDokter/page";
+import KelolaPromo from "@/view/admin/KelolaPromo/page";
+import KelolaEvent from "@/view/admin/KelolaEvent/page";
+import KelolaReservasi from "@/view/admin/KelolaReservasi/page";
+import KelolaKategoriProduk from "@/view/admin/KelolaKategoriProduk/page";
+import Page404 from "@/view/components/Page404/page";
 
 export default function AppRoutes() {
   return (
